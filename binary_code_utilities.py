a844180bf3a012e99474b71d545012f11559cd85
Weronika Patena <patena@gmail.com>
2011-06-21 19:30:38 -0700

    Implemented the Binary_code class; some bug fixes.
    A Binary_code instance is a set of binary codewords of specified length,
    which can be given as a list, read from a file, or generated using a
    generator matrix (passed directly or read from a file).
    Implemented all the initiation/reading/adding methods, simple things
    like size and __str__.
    Re-wrote the previously written fuctions for finding the min and max
    Hamming distance and for finding the bit sum distribution (i.e.  how
    many codewords have what sum of bits) as class methods.
    TODO: Now need to implement manipulation, such as adding a parity bit or
    filtering by the sum or Hamming distance (added empty prototypes for
    those).
    
    Added __xor__ etc to Binary_codeword so that it actually implements
    the bitwise operators I need!  (just let them act on the underlying
    bitstrings)
    
    Tested everything on some simple codes and the [19,10,5] code from a
    generator matrix.  It all seems to work.

--- binary_code_utilities.py
+++ binary_code_utilities.py
@@ -4,6 +4,8 @@ Various utilities for dealing with binary codes: representation, reading from a 
 """
 
 from collections import defaultdict
+from itertools import combinations
+from numpy import array, dot
 import bitstring
 
 class BinaryCodeError(Exception):
@@ -11,7 +13,7 @@ class BinaryCodeError(Exception):
     pass
 
 ### Binary string representations and basic functions
-# what's a sensible representation of binary data?  Certainly not strings.  For 1-dimensional binary codes I suppose I can just use integers... Not really, because int('000',2) is the same as int('0',2)
+# what's a sensible representation of binary data?  Certainly not strings.  For 1-dimensional binary strings I suppose I can just use integers... Not really, because int('000',2) is the same as int('0',2)
 #  http://stackoverflow.com/questions/142812/does-python-have-a-bitfield-type
 # should support basic bitwise operators: &, |, ^, ~
 
@@ -23,9 +25,10 @@ class Binary_codeword:
     def __init__(self,val,length=0,check_length=False):
         """ Generate the self.codeword binary word based on val; pad with 0s on the left to desired length if specified.
         If check_length is True, instead of padding make sure the length is as specified, raise BinaryCodeError if not.
-        If val is a 0/1 string, strip spaces/newlines and convert straight to a bit-string.
-        If val is a list of 0/1 or True/False values, 
-        If val is an int, the bitstring should be as if the builtin bin function was used and the initial 0b stripped.
+        If val is a 0/1 string, strip spaces/newlines and convert straight to a binary string.
+        If val is an int, act as if the builtin bin() function was used to convert to a string (stripping initial 0b).
+        If val is a list of 0/1 or True/False values, treat it as the corresponding 0/1 string.
+        If val is a Binary_codeword instance, just copy it.
         How other argument types will behave is not guaranteed - depends on the package used for the representation."""
         # for binary strings just specify it's bin: '110' and '0b110' and '  110\n' and '11 0' all give the same result
         if isinstance(val,str):   self.codeword = bitstring.BitArray(bin=val)
@@ -35,7 +38,6 @@ class Binary_codeword:
             else:       self.codeword = bitstring.BitArray(bin(val))
         # lists of 0/1 or True/False values natively work as expected; I don't know or care what happens with other types.
         else:                       self.codeword = bitstring.BitArray(val)   
-        self.codeword = bitstring.BitArray(val)
         # pad to given length or check the length if necessary
         if length and not length==len(self):
             if not check_length:
@@ -45,12 +47,10 @@ class Binary_codeword:
                     raise BinaryCodeError("The created binary codeword didn't match the expected length!")
 
     def pad(self,length,value=0):
-        """ Pad on the left to the given length (with 0 by default). """
-        length_diff = length - len(self)
-        # if the length is already correct, do nothing; if it's too high, complain
-        if length_diff==0:      pass
-        elif length_diff < 0:   raise BinaryCodeError("Can't pad the codeword to a length lower than its current length!")
-        else:                   self.codeword.prepend('0b' + length_diff * str(int(value)))
+        """ Pad on the left to the given length (with 0s by default); raise BinaryCodeError if it's already longer."""
+        if length-len(self) < 0:
+            raise BinaryCodeError("Can't pad the codeword to a length lower than its current length!")
+        self.codeword.prepend((length-len(self))*[value])
 
     def check_length(self,length):
         """ Pad on the left to the given length (with 0 by default). """
@@ -64,9 +64,21 @@ class Binary_codeword:
         """ Return a plain 0/1 string representation. """
         return self.codeword.bin[2:]
 
+    def list(self):
+        """ Return a representation as a list of ints (0 or 1). """
+        return [int(x) for x in list(self.codeword)]
+
     def __len__(self):
         """ Return the length of the codeword."""
-        return self.codeword.length()
+        return self.codeword.length
+
+    def __xor__(self,other):    return Binary_codeword(self.codeword ^ other.codeword)
+    def __and__(self,other):    return Binary_codeword(self.codeword & other.codeword)
+    def __or__(self,other):     return Binary_codeword(self.codeword | other.codeword)
+    def __invert__(self):       return Binary_codeword(~self.codeword)
+
+    def __repr__(self):     return "Binary_codeword('%s')"%self.string()
+    def __str__(self):      return self.string()
 
 def Hamming_distance(val1,val2):
     """ Given two binary strings, return the number of bits by which their binary representations differ. """
@@ -76,69 +88,115 @@ def Hamming_distance(val1,val2):
 
 ### Binary code (set of binary strings) representation
 
-class Binary_code:
-    # TODO implement the rest of this!  Just started
+# TODO figure out a naming that won't confuse people!  (Or me!)  Mathematically a "code" is a set of codewords (or a method of encoding things), but IRL a "code" can be either a method of encoding things or just a string, so code/codeword is confusing and even I'm using them wrong!
 
-    def __init__(self,length):
+class Binary_code:
+    """ Essentially a set of Binary_codeword objects, all of the same length."""
+
+    def __init__(self,length,val=[],method='list',expected_count=0):
+        """ Initialize with the given length, and add all elements of values to the set of codewords (default empty)."""
         self.length = length
+        self.method = method
         self.codewords = set()
+        if method=='list':
+            for x in val: self.add(x)
+        elif method=='listfile':    
+            self.read_code_from_file(val)
+        elif method=='matrix':    
+            self.get_code_from_generator_matrix(generator_matrix=val)
+        elif method=='matrixfile':    
+            self.get_code_from_generator_matrix(generator_file=val)
+        else:
+            raise BinaryCodeError("method %s passed to the Binary_code initializer not recognized!"%method 
+                                  + " (allowed methods are list, listfile, matrix, matrixfile)")
+        if expected_count and not self.size()==expected_count:
+            raise BinaryCodeError("Binary_code initializer gave %s codewords,"%self.size() 
+                                  + " not %s as expected!"%expected_count)
 
     def add(self,val):
-        new_word = binary_codeword(val)
-        if not len(new_word)==self.length: 
-            raise BinaryCodeError("Trying to add a codeword of the wrong length to a binary code!")
-        self.codewords.add(new_word)
+        """ Add Binary_code(val) codeword to the code, checking for correct length."""
+        # it's all right if val is a Binary_codeword already, that works too
+        self.codewords.add(Binary_codeword(val,length=self.length,check_length=True))
 
     def size(self):
+        """ Return the number of codewords currently in the code."""
         return len(self.codewords)
 
-    # TODO some of the more complex functions from below should actually be in this class
+    def read_code_from_file(self,infile,expected_count=0):
+        """ Populate the code with codewords read from a plaintext file of 0/1 strings (one per line).
+        Skip comment lines (starting with #).  Optionally make sure the codeword count is as expected. """
+        read_count = 0
+        for line in open(infile):
+            if not line.startswith('#'):
+                self.add(line)
+                read_count += 1
+        if expected_count and not read_count==expected_count:   
+            raise BinaryCodeError("File %s contained %s codewords,"%(infile,read_count)
+                                  + " not %s as expected!"%expected_count)
 
+    def get_code_from_generator_matrix(self,generator_file=None,generator_matrix=None,expected_count=0):
+        """ Given either a generator matrix (as a numpy array) or a plaintext file containing the matrix, 
+        add all codewords generated by that matrix to the current code.  Check if the count is as expected, if given.
+        More information on how a generator matrix works: http://en.wikipedia.org/wiki/Generator_matrix."""
+        # make sure we didn't get a file AND an arra; read the matrix from the file if it was provided as a file
+        if generator_file and generator_matrix:
+            raise BinaryCodeError("Provide either a generator_file or a generator_matrix, not both!")
+        if generator_file:
+            generator_matrix = array([[int(x) for x in line.strip()] for line in open(generator_file)])
+        # the encoded word length and the codeword length (i.e. the n and k from the [n,k,d] description)
+        #  can be inferred from the generator matrix size; check if they match self.length and expected_count (if given)
+        input_code_length, output_code_length = generator_matrix.shape
+        codeword_count = 2**input_code_length
+        if not self.length==output_code_length:
+            raise BinaryCodeError("Trying to use a generator matrix of shape %s to generate codewords of length %s\
+                                  - the sizes don't match up!")%(generator_matrix.shape,self.length)
+        if expected_count and not expected_count==codeword_count:
+            raise BinaryCodeError("The number of codewords generated by the given matrix will be %s, \
+                                  not %s as expected!")%(codeword_count,expected_count)
+        # generate all possible input codewords of length k, and convert them to codewords using matrix dot-multiplication
+        #   (need to do a %2 at the end because binary addition in coding theory is basically xor - I think...)
+        for x in range(codeword_count):
+            x = Binary_codeword(x,length=input_code_length).list()
+            self.add(dot(x,generator_matrix)%2)
 
-### Reading/writing files, generating codes from a generator matrix, etc
+    # TODO I could make one or both of these be the initialization signature instead, but who cares
+    # TODO could add the minimum Hamming distance to this?
+    def __str__(self):  return "<Binary_code instance of length %s and size %s>"%(self.length,self.size())
+    def __repr__(self): return "<Binary_code instance of length %s and size %s>"%(self.length,self.size())
 
-def read_code_file(infile,length=0,count=0):
-    """ Read a file containing binary codes, skipping comment lines (starting with #), return as a set of integers. 
-    Optionally make sure their length and number is what you expect. """
-    codes = set()
-    for line in infile:
-        if line[0]=='#':    continue
-        code = string_to_binary(line)
-        if length and not len(code)==length:   
-            sys.exit("Error: file %s contained a code %s of length %s, not %s as expected!")%(infile,code,len(code),length)
-        codes.append(code)
-    if count and not len(codes)==count:   
-        sys.exit("Error: file %s contained %s codes, not %s as expected!")%(infile,len(codes),count)
-    return codes
+    def find_Hamming_distance_range(self):
+        """ Return a tuple containing the lowest and highest Hamming distance between all codeword pairs."""
+        # set start lov/high values to impossible extremes to make sure they get overwritten
+        low = self.length+1
+        high = 0
+        for x,y in combinations(self.codewords,2):
+            dist = Hamming_distance(x,y)
+            low = min(low,dist)
+            high = max(high,dist)
+        return low,high
+        # more on Hamming distance comparisons/implementations: http://stackoverflow.com/questions/2420412/search-for-string-allowing-for-one-mismatch-in-any-location-of-the-string
 
+    def find_bit_sum_counts(self):
+        """ Given a list of binary codewords, return the number of codewords with each possible bit-sum value (weight)."""
+        bit_sum_counts = defaultdict(lambda: 0)
+        for codeword in self.codewords:
+            bit_sum_counts[codeword.weight()] += 1
+        return dict(bit_sum_counts)     # convert to a normal dictionary before returning, for cleaner display
 
-### More complex operations on codes
+    def add_parity_bit(self):
+        """ Return a new Binary_code instance generated by adding a parity bit to the current codewords.
+        The new Binary_code will have the same number of codewords, a length higher by 1, and a minimum Hamming distance
+        equal to the current one if the current one is even, or higher by one if the current one is odd."""
+        # TODO implement!  Should return a new Binary_code instance with a length higher by 1, or fail if the minimum distance isn't odd?
+        pass
 
-def check_min_max_Hamming_distances(codes, min_distance=-1, max_distance=-1):
-    """ Given a sequence of binary codes, check that the Hamming distance for each pair is between min and max.
-    (if min or max is set to -1, don't check that side)."""
-    # define check functions so we don't have to check for min/max=-1 at each iteration
-    if min_distance==-1:    check_min = lambda x: True
-    else:                   check_min = lambda x: x>=min_distance
-    if max_distance==-1:    check_min = lambda x: True
-    else:                   check_min = lambda x: x<=max_distance
-    N = len(codes)
-    # convert codes to a list so it has a defined order - it may have started as a set or something
-    codes = list(codes)
-    for i in range(N):
-        for j in range(i+1,N):
-            dist = Hamming_distance(codes[i],codes[j])
-            if not check_min(dist) and check_max(dist): return False
-    return True
-    # more on Hamming distance comparisons/implementations: 
-    #  http://stackoverflow.com/questions/2420412/search-for-string-allowing-for-one-mismatch-in-any-location-of-the-string
+    def filter_by_bit_sum(self,low,high):
+        """ Remove all codewords with bit sums outside the low-high range."""
+        # TODO implement
+        pass
 
-def find_bit_sum_counts(codes):
-    """ Given a list of binary codes, return the number of codes with each possible bit-sum value."""
-    bit_sum_counts = defaultdict(lambda: 0)
-    for code in codes:
-        bit_sum_counts[bitwise_sum(code)] += 1
-    return bit_sum_counts
-
-
+    def reduce_by_Hamming_distance(self,low,high,min_count):
+        """ Find a subset of at least min_count codewords with the Hamming distance for each pair in the low-hig range. """
+        # TODO implement using clique_find.py
+        pass
 
