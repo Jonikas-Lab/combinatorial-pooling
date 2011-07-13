394e5f9d95780e28ba37b2240f1de04c027263cb
Weronika Patena <patena@gmail.com>
2011-07-12 17:26:48 -0700

    Mostly finished implementing mirored pooling, and general refactoring.
    Implemented the basic functionality:  running the script with -M will
    now generate mirrored-pooling Biomek files (*_mirror.csv) in addition to
    normal ones.  See previous commit for description of mirrored pooling.
    Added a test case with -M (test2; old test2 is now test3); it seems to
    work.
    
    This involved a lot of refactoring.
    The samples_and_code_to_Biomek_file, which used to do too much, has been
    split up.  Its three main functionalities are now separate: assigning
    codewords to samples and checking that the binary code used makes sense
    with the sample/pool numbers (moved to new assign_codewords), assigning
    plate/well positions to samples and pools (moved to run_main_function),
    and generating a Biomek command list from the above data (moved to new
    codeword_and_position_lists_to_Biomek_file).
    The new codeword_and_position_lists_to_Biomek_file function only takes a
    sample_codeword list with sample_position and pool_position list and
    returns a Biomek command list, instead of doing everything at once.
    The new assign_codewords function simply returns a list of
    Binary_codeword objects, in order, matching the number of samples; it
    takes the input of get_binary_code so we're not passing full binary
    codes around too much.
    
    In addition, instead of having a single list combining sample numbers,
    positions and codewords, we now have simple sample_codewords and
    sample_positions lists; same with pool_positions.
    Rewrote write_data_to_outfile and some other functions to deal with the
    new data formats.
    Also rewrote generate_outfile_names to include names for the mirror
    Biomek files.
    
    All this allows mirroring to be done by simply inverting the codewords
    in the sample_codewords list and then running the next functions as in
    the normal case: codeword_and_position_lists_to_Biomek_file and then
    write_data_to_Biomek_files.  No separate general outfile is made for
    mirrored pooling - no need for one.
    
    Since the samples_and_code_to_Biomek_file function is gone, I moved most
    of its documentation to the module docstring, which makes more sense
    anyway.  The run_main_function just lists its arguments and refers you
    to the module docstring.
    
    TODO: still need to add unit tests for new/modified functions.
    
    TODO: need to implement the option of naming the mirrored pool
    plates something else than the original pool plates.

--- robotic_plate_transfer.py
+++ robotic_plate_transfer.py
@@ -1,17 +1,24 @@
 #!/usr/bin/env python2
 """
-Given a number of samples, a number of pools, a binary code, and information about the physical plates used for the samples and pools, assign one codeword from the code to each sample.  Output two files:  a general output file containing all the script information and the plate/well positions and codewords for each sample (and plate/well positions for each pool), and a Biomek robot transfer file that when given the appropriate plates will execute the combinatorial pooling.
+Generate Biomek transfer files to combine N samples into M pools, using a provided binary code.
 
-For more details see help for the samples_and_code_to_Biomek_file function in this file and the option help descriptions.
+Given a number of samples, a number of pools, a binary code, and information about the physical plates used for the samples and pools, assign one codeword from the code to each sample.  Write a varying number of Biomek robot transfer files (depends on -m/-o and -M options) that when given the appropriate plates will execute the combinatorial pooling.  Also write a general output file containing all the script information, the plate/well positions and codewords for each sample, and the plate/well positions for each pool. 
+
+Assumes the samples will be in sequential order over the provided number of same-sized input plates: [plate1 well A1, plate1 well A2, ..., plate1 well B2, .., plate1 well A1, ...].  The pools will be similarly distributed over the provided number of output plates.  The number and size of output plates does not need to have any particular relationship to the number and size of input plates.
+
+Arbitrarily assigns codewords from the provided binary code to each sample; the pools correspond to each bit of the codeword.  Whenever a bit of the codeword for the sample is 1, that sample should be added to the corresponding pool, so a line of the form "sample_plate_name,sample_plate_well,pool_plate_name,pool_plate_well,volume" is added to the Biomek command list. 
+
+For more details on inputs and available options, run script with -h, or see help strings in define_option_parser function.
 
  -- Weronika Patena, Jonikas lab, Carnegie Institution, July 2011
 
 USAGE:  robotic_plate_transfer.py [options] outfile_base_name
-        robotic_plate_transfer.py -t
+        robotic_plate_transfer.py [-h] [-t] [-T]
 """
 
 import sys
 from collections import defaultdict
+import binary_code_utilities
 from general_utilities import invert_list_to_dict, save_line_list_as_file, write_header_data
 
 from string import ascii_uppercase as letters    # this is 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
@@ -86,65 +93,28 @@ def numbers_to_plate_and_well_IDs(N_samples, plate_size, N_plates, plate_IDs):
     return position_list
 
 
-def samples_and_code_to_Biomek_file(N_samples, N_pools, binary_code, volume,
-                                    input_plate_N, input_plate_size, input_plate_names, 
-                                    output_plate_N, output_plate_size, output_plate_names):
-    """ Make a Biomek transfer file to combine N samples into M pools, using a provided binary code.
-         
-        Assumes the samples will be in sequential order over the provided number of same-sized input plates: 
-        [plate1 well A1, plate1 well A2, ..., plate1 well B2, .., plate1 well A1, ...].
-        The pools will be similarly distributed over the provided number of output plates.  The number and size of 
-        output plates does not need to have any particular relationship to the number and size of input plates.
+def codeword_and_position_lists_to_Biomek_file(sample_codewords, sample_positions, pool_positions, volume):
+    """ Return a list of Biomek transfer commands to perform combinatorial pooling based on sample_codewords.
 
-        The two _plate_names arguments can be provided as a list with the appropriate number of elements, 
-        as a string with the appropriae number of elements separated by commas, or as a single-word string 
-        (in which case sequential numbers will be appended to it to generate the plate names).
+    Inputs:
+     - sample_codewords - sequential list of Binary_codeword object corresponding to each sample
+     - sample_positions - list of plate/well position strings (like "Source1,A4") for each sample, in the same order
+     - pool_positions - same-format list of pool position strings
+     - volume - integer giving the volume of all the transfers 
 
-        Arbitrarily assigns codewords from the code to each sample; the pools correspond to each bit of the codeword. 
-        Whenever a bit of the codeword for the sample is 1, that sample should be added to the corresponding pool, 
-        so a line of the form "sample_plate_name,sample_plate_well,pool_plate_name,pool_plate_well,volume" is added to 
-        the Biomek command list. 
+    Combinatorial pooling: the pools correspond to each bit of the codeword.  Sample A should be added to pool X
+    whenever bit X of the codeword for sample A is 1. 
+    Biomek command list format:  a list of strings of the form "plateA,wellA,plateX,wellX,volume" 
+    where "plateA,wellA" is the value of sample_positions[A], and "plateX,wellX" is pool_positions[X]. """
 
-        Return the Biomek transfer file command list, a list of (input_sample_number,plate_and_well_position_string,
-        codeword) tuples to keep track of which sample corresponds to which codeword, and a list of (pool_number,
-        plate_and_well_position_string) tuples to keep track of pool positions (the last is not strictly necessary, 
-        since the pools will always be in sequential order over the plates/wells, but it can be helpful to have a list 
-        instead of regenerating it every time). """
-
-    if not N_pools == binary_code.length:
-        raise PlateTransferError("The codeword length in the provided binary code must be equal to N_pools!")
-    if N_samples > binary_code.size():
-        raise PlateTransferError("N_samples cannot exceed the size of the provided binary code!")
-    if binary_code.remove_all_zero_codeword():
-        print("Warning: the binary code provided contained the all-zero codeword, which cannot be used, "
-              + "as it would result in a sample being added to none of the pools and thus not sequenced. "
-              + "The all-zero codeword was removed.")
-        if N_samples > binary_code.size():
-            raise PlateTransferError("After removing the all-zero codeword, there aren't enough codewords for all "
-                                     + "samples! Aborting. You could consider inverting the code to get around this.")
-    if N_samples < binary_code.size():
-        print("Warning: N_samples is lower than the size of the provided binary code - an arbitrary subset of codewords "
-              + "will be used.  You may want to reduce your code size manually for improved attributes.")
-
-    # generate plate names from strings if they weren't given as lists
-    input_plate_names = get_plate_name_list_from_input(input_plate_N, input_plate_names)
-    output_plate_names = get_plate_name_list_from_input(output_plate_N, output_plate_names)
-
-    # generate the plate+well position strings for each input sample and each output pool
-    sample_positions = numbers_to_plate_and_well_IDs(N_samples, input_plate_size, input_plate_N, input_plate_names)
-    pool_positions = numbers_to_plate_and_well_IDs(N_pools, output_plate_size, output_plate_N, output_plate_names)
-    # keep track of sample - position - codeword correspondences!  Keep a list of tuples, print to file at the end.
-    sample_number_position_codeword_list = zip(range(N_samples), sample_positions, sorted(list(binary_code.codewords)))
     transfer_file_command_list = []
-    for (sample_number, sample_position, sample_codeword) in sample_number_position_codeword_list:
+    if not set([len(x) for x in sample_codewords]) == set([len(pool_positions)]):
+        raise PlateTransferError("Not all codeword lentgths match the number of pools *%s)!"%len(pool_positions))
+    for (sample_number, (sample_codeword, sample_position)) in enumerate(zip(sample_codewords,sample_positions)):
         pools_to_add_sample_to = [pool_number for (pool_number,if_add) in enumerate(sample_codeword.list()) if if_add==1]
         for pool_number in pools_to_add_sample_to:
             transfer_file_command_list.append("%s,%s,%s"%(sample_position,pool_positions[pool_number],volume))
-    # make a list of output pools and positions to return
-    pool_number_position_list = list(enumerate(pool_positions))
-    # for the return, we want the codewords to be strings, not objects
-    sample_number_position_codeword_list = [(n,p,c.string()) for (n,p,c) in sample_number_position_codeword_list]
-    return transfer_file_command_list, sample_number_position_codeword_list, pool_number_position_list
+    return transfer_file_command_list
 
 
 def get_plate_name_list_from_input(N_plates,ID_input):
@@ -245,9 +215,9 @@ def test_functionality():
                                            PlateTransferError, message="Shouldn't work, 10 isn't a valid plate size!")
         print "...DONE"
 
-    if True:
+    # TODO this function no longer exists; keeping code until I can switch it to test the new functions
+    if False:   
         print "Testing samples_and_code_to_Biomek_file function... (ignore any warnings)"
-        import binary_code_utilities
         B1 = binary_code_utilities.Binary_code(2,['01','10','11','00'])
         command_list, sample_list, pool_list = samples_and_code_to_Biomek_file(3,2,B1,20, 1,24,['Src1'], 1,6,['Dest1'])
         assert command_list == ['Src1,A1,Dest1,A2,20', 'Src1,A2,Dest1,A1,20', 'Src1,A3,Dest1,A1,20', 'Src1,A3,Dest1,A2,20']
@@ -323,7 +293,6 @@ def define_option_parser():
 
 def get_binary_code(listfile=None,matrixfile=None):
     """ Given a listfile or matrixfile name (but not both!), return the generated binary code."""
-    import binary_code_utilities
     if not (listfile or matrixfile):
         raise PlateTransferError("You must provide either a listfile or a matrixfile to generate a binary code!")
     if listfile and matrixfile:
@@ -359,6 +328,34 @@ def check_options_and_args(parser,options,args):
     # MAYBE-TODO could allow -p/-P to be automatically calculated from -n/-N and -s/-S?
 
     return options,outfile_basename
+
+
+def assign_codewords(N_samples, N_pools, binary_code):
+    """ Returns a list of Binary_codeword objects, of length N_samples - one codeword per sample, ordered. """
+
+    if not N_pools == binary_code.length:
+        raise PlateTransferError("The codeword length in the provided binary code must be equal to N_pools!")
+    if N_samples > binary_code.size():
+        raise PlateTransferError("N_samples cannot exceed the size of the provided binary code!")
+    if binary_code.remove_all_zero_codeword():
+        print("Warning: the binary code provided contained the all-zero codeword, which cannot be used, "
+              + "as it would result in a sample being added to none of the pools and thus not sequenced. "
+              + "The all-zero codeword was removed.")
+        if N_samples > binary_code.size():
+            raise PlateTransferError("After removing the all-zero codeword, there aren't enough codewords for all "
+                                     + "samples! Aborting. You could consider inverting the code to get around this.")
+    if N_samples < binary_code.size():
+        print("Warning: N_samples is lower than the size of the provided binary code - an arbitrary subset of codewords "
+              + "will be used.  You may want to reduce your code size manually for improved attributes.")
+
+    # make the codewords into a sorted list, to guarantee the result is always the same
+    codewords = sorted(binary_code.codewords)
+    # we may as well do something useful here - remove the codewords with highest weight, to avoid issues like the all-one codeword and generally to limit the number of samples per pool.  MAYBE-TODO pick another way?  Make it an option?
+    codewords.sort(key = lambda codeword: codeword.weight())
+    # pick the required number of codewords
+    sample_codewords = codewords[:N_samples]
+    return sample_codewords
+    # TODO add this to the unit-test!
 
 
 def generate_outfile_names(outfile_basename,if_multiple_files,if_mirror_files,number_of_files=None,file_plate_names=None):
@@ -420,41 +417,56 @@ def write_data_to_Biomek_files(outfiles_Biomek, transfer_file_command_list, Biom
             save_line_list_as_file(data, outfilename, header=Biomek_header)
 
 
-def write_data_to_outfile(sample_number_position_codeword_list, pool_number_position_list, 
+def write_data_to_outfile(sample_codewords, sample_positions, pool_positions, 
                           outfile_data, outfiles_Biomek, options=None):
     """ Print data to main outfile: header information (command, path, date/time, options - all as #-start comments), 
-    the sample_number_position_codeword_list, and the pool_number_position_list. """
+    sample numbers/positions/codewords, and pool numbers/positions. """
     # print all the usual header information (command, path, date/time, options)
     OUTFILE = open(outfile_data,'w')
     write_header_data(OUTFILE,options)
     OUTFILE.write("# Corresponding Biomek file(s): %s\n"%outfiles_Biomek)
     # write the sample_number_position_codeword_list and pool_number_position_list data
     OUTFILE.write("\nsample_number,\tplate_and_well_position,\tcodeword\n")
-    for (n,p,c) in sample_number_position_codeword_list:
-        OUTFILE.write("%s,\t%s,\t%s\n"%(n,p,c))
+    for (number,(codeword,position)) in enumerate(zip(sample_codewords,sample_positions)):
+        OUTFILE.write("%s,\t%s,\t%s\n"%(number,position,codeword.string()))
     OUTFILE.write("\npool_number,\tplate_and_well_position\n")
-    for (n,p) in pool_number_position_list:
-        OUTFILE.write("%s,\t%s\n"%(n,p))
+    for (number,position) in enumerate(pool_positions):
+        OUTFILE.write("%s,\t%s\n"%(number,position))
     OUTFILE.close()
 
 
 def run_main_function(parser,options,args):
-    """ Run the main functionality of the module (see module and samples_and_code_to_Biomek_file docstrings).
+    """ Run the main functionality of the module (see module docstring for more information).
     Takes an optparse parser object, and the options object and arg list generated by the parser."""
+    # MAYBE-TODO may be more convenient for interactive use if this just took an input string, and generated/defined the parser itself...
     options,outfile_basename = check_options_and_args(parser,options,args)
-    (outfile_data, outfiles_Biomek, outfiles_Biomek_mirror) = generate_outfile_names(outfile_basename, options.multiple_Biomek_files,options.add_mirror_pooling_files, options.number_of_sample_plates, options.sample_plate_IDs)
-    print "Output files: %s and %s"%(outfile_data, outfiles_Biomek+outfiles_Biomek_mirror)
-    binary_code = get_binary_code(options.binary_code_list_file, options.binary_code_generator_file)
-    args = (options.number_of_samples, options.number_of_pools, binary_code, options.volume_per_transfer, 
-            options.number_of_sample_plates, options.size_of_sample_plates, options.sample_plate_IDs, 
-            options.number_of_pool_plates, options.size_of_pool_plates, options.pool_plate_IDs)
-    (transfer_file_command_list, sample_info_list, pool_info_list) = samples_and_code_to_Biomek_file(*args)
-    write_data_to_outfile(sample_info_list, pool_info_list, outfile_data, outfiles_Biomek+outfiles_Biomek_mirror, options)
+    outfiles = generate_outfile_names(outfile_basename, options.multiple_Biomek_files, options.add_mirror_pooling_files, 
+                                      options.number_of_sample_plates, options.sample_plate_IDs)
+    (outfile_data, outfiles_Biomek, outfiles_Biomek_mirror) = outfiles
+    print("Output files: %s"%(outfiles,))
+    # assign codewords to samples
+    sample_codewords = assign_codewords(options.number_of_samples, options.number_of_pools, 
+                                       get_binary_code(options.binary_code_list_file, options.binary_code_generator_file))
+    # generate plate names from strings if they weren't given as lists
+    input_plate_names = get_plate_name_list_from_input(options.number_of_sample_plates, options.sample_plate_IDs)
+    output_plate_names = get_plate_name_list_from_input(options.number_of_pool_plates, options.pool_plate_IDs)
+    # generate the plate+well position strings for each input sample and each output pool
+    sample_positions = numbers_to_plate_and_well_IDs(options.number_of_samples, options.size_of_sample_plates, 
+                                                     options.number_of_sample_plates, input_plate_names)
+    pool_positions = numbers_to_plate_and_well_IDs(options.number_of_pools, options.size_of_pool_plates, 
+                                                   options.number_of_pool_plates, output_plate_names)
+    # generate the Biomek transfer command list based on sample codewords and sample/pool positions
+    transfer_file_command_list = codeword_and_position_lists_to_Biomek_file(sample_codewords, sample_positions, 
+                                                                            pool_positions, options.volume_per_transfer)
+    # write to outfiles
     write_data_to_Biomek_files(outfiles_Biomek, transfer_file_command_list, options.Biomek_file_header)
+    write_data_to_outfile(sample_codewords, sample_positions, pool_positions, 
+                          outfile_data, outfiles_Biomek+outfiles_Biomek_mirror, options)
     if options.add_mirror_pooling_files:
-        # TODO implement mirror_transfer_file_command_list!!  Using the Binary_code.__invert__ method (~x)?... Actually that can't be done based on transfer_file_command_list, that's all strings.  I guess base it on sample_info_list, and refactor/reuse code from samples_and_code_to_Biomek_file to make it happen?  Yes, basically split samples_and_code_to_Biomek_file into a function that assigns codewords to samples (and a different or same function that assigns plate/well position strings to samples and pools?) and a function that generates a transfer command file from a given sample+codeword list.  Then we can easily insert a mirroring step between the first and second function.
-        # TODO what should the destination plate names be for the mirrored set?  Spencer says they should be different from the normal ones (I guess just add a 'mirror' suffix or something)
-        mirror_transfer_file_command_list = make_mirror_transfer_commands(transfer_file_command_list)
+        mirror_codewords = [~codeword for codeword in sample_codewords]
+        # TODO what should the destination plate names be for the mirrored set?  Spencer says they should be different from the normal ones (I guess just add a 'mirror' suffix or something) - write something to do that!
+        mirror_transfer_file_command_list = codeword_and_position_lists_to_Biomek_file(mirror_codewords, sample_positions, 
+                                                                            pool_positions, options.volume_per_transfer)
         write_data_to_Biomek_files(outfiles_Biomek_mirror, mirror_transfer_file_command_list, options.Biomek_file_header)
 
 
